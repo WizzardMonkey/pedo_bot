3bfa5233b1456752a5843a0fab96867f4b6a48f0
wizzardmonkey <rstidade@hotmail.com.br>
2017-01-16 11:31:29 -0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,37 +1,46 @@
-#!/usr/bin/python
-import discord
-import os.path
-import aiohttp
+#bot discord
+#criado por Mr Monkey
 import asyncio
+import aiohttp
+import logging, traceback
+import sys, re, io, enum
+import tempfile, os, hashlib
+import itertools
+import datetime
+from collections import namedtuple
+from os.path import split as path_split
 from time import ctime
-
-
+import discord
 client = discord.Client()
 
 import requests.packages.urllib3
 requests.packages.urllib3.disable_warnings()
 
-def clear_chat_channel(message):
-    counter = 0
-    all_messages = client.messages
-    target_channel = message.channel
-    for message_step in all_messages:
-        if message_step.channel == target_channel:
-            client.delete_message(message_step)
-            counter += 1
-    client.send_message(message.channel, 'I have removed %s old messages' % counter)
-#
+@asyncio.coroutine
+
+
+async def clear(message):
+  deletar = message.content.strip("/clear")
+  deletarint = int(deletar)
+  await client.purge_from(message.channel, limit=deletarint)
+  await client.send_message(message.channel, "🗑️ |``Eu deletei " + deletar + " mensagens``")
 async def quit(message):
   await client.send_message(message.channel, "QUER MESMO SAIR ?(y/n)")
 
 async def time(message):
   time = ctime()
-  await client.send_message(message.channel, time)
+  await client.send_message(message.channel, "⏰ " + time)
 #
 async def say(message):
    say = message.content.strip("/say")
    await client.send_message(message.channel, say)
 
+async def radio():
+    #aqui o bot entra no canal logo quando o bot entra no servidor.
+    #await client.send_message(message.channel, "bot entrou no canal de audio")
+
+    canal_voz = discord.Object(id='12324234183172')#canal de voz em que o bot entra.(ID)
+    await client.join_voice_channel(canal_voz)
 
 #
 
@@ -39,7 +48,7 @@ async def xingar(message):
    pessoa = message.content.strip("/xingar")
    marcar = pessoa
    await client.send_message(message.channel, "Voce tera cancer" + marcar)
-   
+
 #
 async def what(message):
    await client.send_message(message.channel, "¯\_(ツ)_/¯")
@@ -48,13 +57,21 @@ async def what(message):
 async def resposta_teste(message):
    await client.send_message(message.channel, "Iae!")
 
-#
 async def info(message):
    await client.send_message(message.channel, "```Bot da pedo bear, links:```")
 
 
 async def help(message):
-   await client.send_message(message.channel, "```Comandos: /time (horario, dia, etc) /spam (message) /say (message) /xingar (author) /clear (numero de mensagens para apagar)```")
+   await client.send_message(message.channel,
+   """
+   ```Comandos:
+   /time (horario, dia, etc)
+   /spam (message)
+   /say (message)
+   /xingar (author)
+   /clear (numero de mensagens para apagar)```
+   """)
+
 
 async def spam(message):
     i = 1
@@ -65,7 +82,6 @@ async def spam(message):
 @client.event
 async def on_message(message):
     author = message.author
-
     if author == client.user:
        return # nao responder as propias mensagens
 
@@ -94,12 +110,12 @@ async def on_message(message):
     if message.content.startswith("/quit"):
         await quit(message)
     if message.content.startswith('/clear'):
-        clear_chat_channel(message)
+        await clear(message)
+
 
 @client.event
 async def on_ready():
+        await client.change_presence(game=discord.Game(name="/help para ajuda"))
+        radio()
         client.accept_invite("https://discordapp.com/oauth2/authorize?client_id=269166666519281664&scope=bot&permissions=2146958463")
-
-
-
-client.run("MjY5MTY2NjY2NTE5MjgxNjY0.C1mCGg.7V_WwlopMvrm2jJ0M8juyd3fDFA")
+        client.run("MjY5MTY2NjY2NTE5MjgxNjY0.C1mCGg.7V_WwlopMvrm2jJ0M8juyd3fDFA")
